7af85f54b75cbf4e9aa83891978276b256a11544
Ben Smith <bsmith.eng@gmail.com>
2014-02-02 12:21:58 -0800

    Added chain/path finding

--- word_chains.rb
+++ word_chains.rb
@@ -9,6 +9,34 @@ class WordChains
     @candidate_words = Set.new []
     self.populate_candidates
     @reachable_words = [start_word]
+    @parents = {}
+  end
+
+  def build_path
+    path = []
+    current_word = @target_word
+    until current_word == @start_word
+      p current_word
+      path << current_word
+      current_word = @parents[current_word]
+    end
+    path << @start_word
+    path.reverse
+  end
+
+  def find_chain
+    current_word = ""
+    until @words_to_expand.empty? || current_word == @target_word
+      current_word = @words_to_expand.shift
+      adjacent_words = self.adjacent_words(current_word)
+      adjacent_words.each do |word|
+        @words_to_expand << word
+        @reachable_words << word
+        @candidate_words.delete(word)
+        @parents[word] = current_word
+      end
+    end
+    @reachable_words.include?(@target_word) ? build_path : nil
   end
 
   def explore_words
@@ -21,7 +49,6 @@ class WordChains
         @candidate_words.delete(word)
       end
     end
-    p @reachable_words
     @reachable_words.include?(@target_word)
   end
 
@@ -52,5 +79,5 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   rubyduck = WordChains.new("duck", "ruby");nil
-  p rubyduck.explore_words
+  p rubyduck.find_chain
 end
